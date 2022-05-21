2096bc8fd62db9da121258bcd419d38f7bb4618e
Enzo Henrique Hosi Barbosa <EnzoHHB@gmail.com>
2022-05-21 08:23:39 -0300

    Create enzo.js

---
+++ enzo.js
@@ -0,0 +1,22 @@
+const Variable = class { value = null; exp = new Map };
+
+
+const { exp } = 
+
+// Safe getter
+parsed.set(term.var, 
+parsed.get(term.var) || new Variable).get(term.var);
+
+exp.set(term.exp,
+exp.get(term.exp) || []);
+
+
+const scope = { x: 1 };
+
+// Assigning
+for (const [ variable, value ] of scope) 
+    parsed.get(variable).value = value;
+
+
+
+
